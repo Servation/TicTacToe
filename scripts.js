64b358887c101d2bceb70d2dedb57513ca53f271
Servation <gwfds123@gmail.com>
2021-12-29 23:20:22 -0800

    Added player names

--- scripts.js
+++ scripts.js
@@ -1,3 +1,46 @@
+
+const player = (playerName) => {
+    let currentPos = [];
+    let won = false;
+    let name = playerName;
+    const winningPos =
+        [['a1', 'a2', 'a3'],
+        ['b1', 'b2', 'b3'],
+        ['c1', 'c2', 'c3'],
+        ['a1', 'b2', 'c3'],
+        ['a3', 'b2', 'c1'],
+        ['a1', 'b1', 'c1'],
+        ['a2', 'b2', 'c2'],
+        ['a3', 'b3', 'c3']];
+
+    const checkWinning = () => {
+        winningPos.forEach(arr => {
+            if (currentPos.includes(arr[0]) && currentPos.includes(arr[1]) && currentPos.includes(arr[2])) {
+                won = true;
+            }
+        });
+        console.log(won);
+    };
+    const getName = () => name;
+    const setName = (newName) => {
+        name = newName;
+    };
+
+    const getWon = () => won;
+    const setPos = (pos) => {
+        currentPos.push(pos);
+    };
+    const getPos = () => currentPos;
+    const checkPos = (pos) => currentPos.includes(pos);
+    const reset = () => {
+        currentPos = [];
+        won = false;
+    };
+    return { setPos, getPos, reset: reset, checkPos, checkWinning, getWon, getName, setName };
+}
+const playerOne = player("Player X");
+const playerTwo = player("Player O");
+
 const gameBoard = (() => {
     let _board = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
     const getBoard = () => {
@@ -9,7 +52,9 @@ const gameBoard = (() => {
 const displayController = (() => {
     let PlayerOneTurn = true;
     let tieCounter = 0;
-    const boardEl = document.getElementById("GameBoard")
+    const boardEl = document.getElementById("GameBoard");
+    const resetButton = document.getElementById("restartButton");
+    const gameINFO = document.getElementById("info");
     const resetBoard = () => {
         let boardNodes = [...document.getElementsByClassName("square")]
         boardNodes.forEach(el => {
@@ -18,7 +63,23 @@ const displayController = (() => {
         playerOne.reset();
         playerTwo.reset();
         tieCounter = 0;
+        if (playerOne.getName() == "") {
+            playerOne.setName("Player X");
+        }
+        if (playerTwo.getName() == "") {
+            playerTwo.setName("Player O");
+        }
     };
+
+    resetButton.addEventListener("click", () => {
+        const player1Input = document.getElementById("Player1");
+        const player2Input = document.getElementById("Player2");
+        playerOne.setName(player1Input.value);
+        playerTwo.setName(player2Input.value);
+        player1Input.value = "";
+        player2Input.value = "";
+        resetBoard();
+    });
     gameBoard.getBoard().forEach(el => {
         let currentEl = document.createElement("div")
         currentEl.setAttribute("id", el);
@@ -42,54 +103,16 @@ const displayController = (() => {
                 }
             }
             if (playerOne.getWon()) {
-                console.log("Player One Won");
+                gameINFO.textContent = playerOne.getName() + " Won";
                 resetBoard();
             } else if (playerTwo.getWon()) {
-                console.log("Player Two Won");
+                gameINFO.textContent = playerTwo.getName() + " Won";
                 resetBoard();
             } else if (tieCounter == 9) {
-                console.log("It was tied")
+                gameINFO.textContent = "It was tied";
                 resetBoard();
             }
         });
         boardEl.appendChild(currentEl);
     });
 })();
-
-const player = () => {
-    let currentPos = [];
-    let won = false;
-    const winningPos =
-        [['a1', 'a2', 'a3'],
-        ['b1', 'b2', 'b3'],
-        ['c1', 'c2', 'c3'],
-        ['a1', 'b2', 'c3'],
-        ['a3', 'b2', 'c1'],
-        ['a1', 'b1', 'c1'],
-        ['a2', 'b2', 'c2'],
-        ['a3', 'b3', 'c3']];
-
-    const checkWinning = () => {
-        winningPos.forEach(arr => {
-            if (currentPos.includes(arr[0]) && currentPos.includes(arr[1]) && currentPos.includes(arr[2])) {
-                won = true;
-            }
-        });
-        console.log(won);
-    };
-    const getWon = () => won;
-
-    const setPos = (pos) => {
-        currentPos.push(pos);
-    };
-    const getPos = () => currentPos;
-    const checkPos = (pos) => currentPos.includes(pos);
-    const reset = () => {
-        currentPos = [];
-        won = false;
-    };
-    return { setPos, getPos, reset: reset, checkPos, checkWinning, getWon };
-}
-
-const playerOne = player();
-const playerTwo = player();
